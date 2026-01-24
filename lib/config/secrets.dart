588c89b4e00413c2639b047aff9fd2d95415cae3
Rahul V S <117752342+techieRahul17@users.noreply.github.com>
2026-01-25 01:29:18 +0530

    Update secrets.dart

--- lib/config/secrets.dart
+++ lib/config/secrets.dart
@@ -1,4 +1,4 @@
 class Secrets {
   // Public Beta Key (Rate Limited). Replace with your own from developers.giphy.com for production.
-  static const String giphyApiKey = 'dc6zaTOxFJmzC';
+  static const String giphyApiKey = 'Generate a key from giphy developers and paste here';
 }
